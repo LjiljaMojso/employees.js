00596d9d2bdeff3b30dc8342f7a5f8f220fb808b
LjiljaMojso <ljmojso@gmail.com>
2022-06-01 18:02:52 +0200

    refactor main function

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ async function getDataSalaries(path, path2) {
   const data2 = await response2.json();
   return employeesWithSalaries(data, data2);
 }
-getDataSalaries("./fake-server/salaries.json", "./fake-server/employees.json")
-  .then((data) => {
-    data.sort((a, b) => a?.salary - b?.salary);
-    console.log(data);
-  })
-  .catch((err) => console.log("error"));
 const employeesWithSalaries = (arr1, arr2) => {
   let temp = [];
 
@@ -24,5 +18,11 @@ const employeesWithSalaries = (arr1, arr2) => {
   });
   return temp;
 };
+getDataSalaries("./fake-server/salaries.json", "./fake-server/employees.json")
+  .then((data) => {
+    data.sort((a, b) => a?.salary - b?.salary);
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
 
 module.exports = getDataSalaries;
